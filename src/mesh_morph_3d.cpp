ed9483ac1372e94b556cbff73c5ed6c733e8d6ca
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-09-29 11:10:42 -0700

    Add license from upstream.

--- src/mesh_morph_3d.cpp
+++ src/mesh_morph_3d.cpp
@@ -1,5 +1,14 @@
 // Copyright (c) 2014-present K. S. Ernest (iFire) Lee.
 // SPDX-License-Identifier: MIT
+//
+//------------------------------------------- DISCLAIMER ----------------------------------------------------//
+// Implementation published as additional material                                                           //
+// It comes without any guarantees. Use it at you own risks.                                                 //
+// In case you use this software, please acknowledge it and cite:                                            //
+//    Biharmonic Coordinates and their Derivatives for Triangular 3D Cages                                   //
+//    Jean-Marc THIERY, Elie MICHEL, Jiong CHEN                                                              //
+//    Siggraph 2024                                                                                          //
+//-----------------------------------------------------------------------------------------------------------//
 
 #include "mesh_morph_3d.h"
 #include "BHC.h"
--- src/mesh_morph_3d.h
+++ src/mesh_morph_3d.h
@@ -1,5 +1,14 @@
 // Copyright (c) 2014-present K. S. Ernest (iFire) Lee.
 // SPDX-License-Identifier: MIT
+//
+//------------------------------------------- DISCLAIMER ----------------------------------------------------//
+// Implementation published as additional material                                                           //
+// It comes without any guarantees. Use it at you own risks.                                                 //
+// In case you use this software, please acknowledge it and cite:                                            //
+//    Biharmonic Coordinates and their Derivatives for Triangular 3D Cages                                   //
+//    Jean-Marc THIERY, Elie MICHEL, Jiong CHEN                                                              //
+//    Siggraph 2024                                                                                          //
+//-----------------------------------------------------------------------------------------------------------//
 
 #ifndef MESH_MORPH_3D_H
 #define MESH_MORPH_3D_H
