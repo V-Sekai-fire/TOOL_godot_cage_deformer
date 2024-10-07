1b339ae72119d155e9e4d61fa55c3e1f0fb89e68
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-10-07 10:10:07 -0700

    Update to the latest VertexHandles Gizmo.

--- demo/addons/VertexHandles/VertexHandles.gd
+++ demo/addons/VertexHandles/VertexHandles.gd
@@ -1,41 +1,101 @@
 @tool
 class_name VertexHandles extends Node3D
 
+## Assign a new [VertexHandles] under a [MeshInstance3D] to modify it's mesh
+
+
+# PUBLIC
+
+@export var wireframe := true : 
+	set(value):
+		wireframe = value
+		_request_redraw.emit()
+@export var wireframe_color := Color.CORNFLOWER_BLUE : 
+	set(value):
+		wireframe_color = value
+		_request_redraw.emit()
+
+# Array of point arrays
+@export var point_arrays := [] : set = _set_points
+
+# The target mesh whose vertices we want to modify
 @onready var mesh = get_parent().mesh
 
-@export var points := [] : set = _set_points
+
+# PRIVATE
+
+signal _request_redraw
+
+# TODO
+# - Add options
+# - Support double vertices
+# - Support multiple surfaces
+# - Make sure things are commiting and can undo/redo 
+# - Fix handle rendering bug
+# - Sort handles from back to front
 
 func _ready() -> void:
 	assert(get_parent() is MeshInstance3D)
+	
+	_refresh_point_arrays()
+	
+	# Refresh the points just in case we are dealing with a new mesh
+	EditorInterface.get_selection().selection_changed.connect(_refresh_point_arrays)
+
+func _refresh_point_arrays():
+	if not self in EditorInterface.get_selection().get_selected_nodes():
+		return
+	
 	get_parent().mesh = _to_array_mesh(get_parent().mesh)
 	mesh = get_parent().mesh
-	points = []
+	
+	point_arrays = []
+	
 	for i in range(0,mesh.get_surface_count()):
 		var arrays = mesh.surface_get_arrays(i)
-		points.push_back(arrays[Mesh.ARRAY_VERTEX])
+		
+		point_arrays.push_back(arrays[Mesh.ARRAY_VERTEX])
 
 func _update_mesh():
-	if is_node_ready() and not points.is_empty():
+	if is_node_ready() and not point_arrays.is_empty():
 		mesh = get_parent().mesh
+		
 		var surface_arrays := []
 		for i in range(0,mesh.get_surface_count()):
 			var arrays = mesh.surface_get_arrays(i)
 			surface_arrays.push_back(arrays)
+		
+		# Cache the primitive type
+		var type : Mesh.PrimitiveType = _get_primitive_type(mesh)
+		
 		mesh.clear_surfaces()
+		
 		for i in range(0,surface_arrays.size()):
-			surface_arrays[i][Mesh.ARRAY_VERTEX] = PackedVector3Array( points[i] )
-			mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_arrays[i])
-		get_node("../../MeshMorph3D").apply_deformation_to_children()
+			surface_arrays[i][Mesh.ARRAY_VERTEX] = PackedVector3Array( point_arrays[i] )
+			
+			mesh.add_surface_from_arrays(type, surface_arrays[i])
+	
+	_request_redraw.emit()
 
 func _set_points(value):
-	points = value
+	point_arrays = value
+	_update_mesh()
 
 func _to_array_mesh(_mesh:Mesh) -> ArrayMesh:
 	var surface_arrays := []
 	for i in range(0,_mesh.get_surface_count()):
 		var arrays = _mesh.surface_get_arrays(i)
 		surface_arrays.push_back(arrays)
+	
+	# Cache the primitive type
+	var type : Mesh.PrimitiveType = _get_primitive_type(_mesh)
+	
 	_mesh = ArrayMesh.new()
+	
 	for i in range(0,surface_arrays.size()):
-		_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_arrays[i])
+		_mesh.add_surface_from_arrays(type, surface_arrays[i])
+	
 	return _mesh
+
+func _get_primitive_type(_mesh:Mesh, id:=0) -> Mesh.PrimitiveType:
+	return RenderingServer.mesh_get_surface(_mesh.get_rid(), 0)["primitive"]
--- demo/addons/VertexHandles/VertexHandlesGizmo.gd
+++ demo/addons/VertexHandles/VertexHandlesGizmo.gd
@@ -2,65 +2,94 @@
 extends EditorNode3DGizmoPlugin
 
 func _init():
-	create_material("main", Color(0,1,1),false,true)
+	create_material("main", Color(1,1,1), false, true, true)
 	create_handle_material("handles",false)
 
 const MyCustomNode3D = preload("VertexHandles.gd")
 func _has_gizmo(node):
 	return node is MyCustomNode3D
 
+func _create_gizmo(for_node_3d: Node3D) -> EditorNode3DGizmo:
+	if not _has_gizmo(for_node_3d):
+		return null
+	
+	var gizmo = EditorNode3DGizmo.new()
+	
+	# Allows the node3d associated with this gizmo to request redraw
+	for_node_3d._request_redraw.connect(_redraw.bind(gizmo))
+	
+	return gizmo
+
+# show gizmo name in visibility list
 func _get_gizmo_name():
 	return "VertexHandlesGizmo"
 
 func _get_handle_name(gizmo,id,secondary):
 	return str(id)
-	match id:
-		0:
-			return "Radius"
-		1:
-			return "Width"
 
 func _get_handle_value(gizmo,id,secondary):
 	var node3d : Node3D = gizmo.get_node_3d()
-	return node3d.points[0][id]
-	match id:
-		0:
-			return node3d.radius
-		1:
-			return node3d.width
-	
+	return node3d.point_arrays[0][id]
+
 func _set_handle(gizmo,id,secondary,camera,point):
 	var node3d : Node3D = gizmo.get_node_3d()
-	var gt : Transform3D = node3d.get_global_transform()
-	var gi : Transform3D = gt.affine_inverse()
+	
+	# Construct the view ray in world space
 	var ray_from : Vector3 = camera.project_ray_origin(point)
 	var ray_dir : Vector3 = camera.project_ray_normal(point)
-	ray_from = node3d.global_transform.affine_inverse() * ray_from
-	ray_dir = node3d.global_transform.affine_inverse().basis * ray_dir
-	var plane = Plane(camera.get_camera_transform().basis[2],node3d.points[0][id])
+	
+	# Intersect the ray with a camera facing plane
+	var plane = Plane(camera.get_camera_transform().basis[2], node3d.global_transform * node3d.point_arrays[0][id])
 	var p = Geometry3D.segment_intersects_convex(ray_from,ray_from+ray_dir*16384,[plane])
+	
 	if p.is_empty():
 		return
+	
 	p = p[0]
-	var d = p.distance_to(node3d.global_position)
-	node3d.points[0][id] = p
+	
+	# Transform the intersection point from world space to local node space
+	p = node3d.global_transform.affine_inverse() * p
+	
+	node3d.point_arrays[0][id] = p
+	node3d.point_arrays = node3d.point_arrays # Force setter call
+	
 	_redraw(gizmo)
-	node3d._update_mesh()
 
 func _commit_handle(gizmo,id,secondary,restore,cancel):
 	var node3d : Node3D = gizmo.get_node_3d()
-
+	
+	# TODO - use EditorUndoRedoManager
 
 func _redraw(gizmo):
 	gizmo.clear()
+	
 	var node3d : Node3D = gizmo.get_node_3d()
-	var lines = PackedVector3Array()
-	lines.push_back(Vector3(1, 1, -1))
-	lines.push_back(Vector3(0, 0, 0))
+		
+	if node3d.wireframe:
+		var lines = PackedVector3Array()
+		
+		var mdt := MeshDataTool.new()
+		
+		for surface_id in range(0,(node3d.mesh as Mesh).get_surface_count()):
+			mdt.create_from_surface(node3d.mesh, surface_id)
+			for face_id in range(0,mdt.get_face_count()):
+				for j in range(0,3):
+					lines.push_back( mdt.get_vertex(mdt.get_face_vertex(face_id,j)) )
+					lines.push_back( mdt.get_vertex(mdt.get_face_vertex(face_id,(j+1)%3 )) )
+		
+		gizmo.add_lines(lines, get_material("main", gizmo), false, node3d.wireframe_color)
+	
+	
 	var handles := PackedVector3Array()
-	for i in range(0, node3d.points.size()):
-		var point_array = node3d.points[i]
+	
+	for i in range(0,node3d.point_arrays.size()):
+		var point_array = node3d.point_arrays[i]
+		
 		for j in range(0,point_array.size()):
 			handles.push_back( point_array[j] )
-	var billboard := false
-	gizmo.add_handles(handles, get_material("handles", gizmo), [], billboard)
+			#print(arrays[Mesh.ARRAY_VERTEX][j])
+	
+	gizmo.add_handles(handles, get_material("handles", gizmo), [], false)
+	
+	
+	#gizmo.set_hidden(not gizmo.is_subgizmo_selected(0))
